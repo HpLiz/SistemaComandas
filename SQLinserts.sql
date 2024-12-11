e85fd87a1fbc0823a8517472beba700f16fc1d53
HpLiz <lizbethhzpz@gmail.com>
2024-12-11 01:31:51 -0600

    inserts sql

---
+++ SQLinserts.sql
@@ -0,0 +1,20 @@
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Verdura', 15.0, 'Verduras fritas');
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Huevo', 5.0, 'Medio huevo hervido');
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Pasta', 35.0, 'Porcion de pasta');
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Aguacate', 10.0, 'Porcion de aguacate');
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Philadelphia', 10.0, 'Queso crema');
+INSERT INTO EXTRA(NOMBRE, PRECIO, DESCRIPCION) VALUES('Carne Tokio', 25.0, 'Carne de cerdo');
+
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Dai Sensei', 'Ramen picante de pollo', 'Comida', 80.0, 'Pasta, pollo, cilantro');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Chikin Katsu', 'Pollo empanizado', 'Comida', 80.0, 'Pollo, arroz, lechuga, sakura souce');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Surimi Roll', 'Sushi de surimi', 'Comida', 70.0, 'Arroz, alga, surimi, queso');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Hibiscas', 'Frappe de jamaica', 'Bebida', 35.0, 'Jamaica, hielo, azucar');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Calpis uva', 'Bebida fermentada', 'Bebida', 30.0, 'Calpis, jugo de uva');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Frappe Totoro', 'Frappe de leche', 'Bebida', 80.0, 'Leche, ajonjoli, hielo');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Chococafe', 'Rebanada de pastel', 'Postre', 35.0, 'Cafe, chocolate, leche');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Choco Banana Pafe', 'Postre combinado', 'Postre', 50.0, 'Helado, pastel japones, crema');
+INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, TIPO, PRECIO, INGREDIENTES) VALUES('Crepa Ichigo', 'Crepa japonesa', 'Postre', 40.0, 'Fresas, chocolate, crema');
+
+INSERT INTO USUARIO(NOMBRE, AMATERNO, APATERNO, PUESTO) VALUES('Haruka', 'Kusano', 'K', 'Cocina');
+INSERT INTO USUARIO(NOMBRE, AMATERNO, APATERNO, PUESTO) VALUES('Michelle', 'Hernandez', 'Cruz', 'Bebidas');
+INSERT INTO USUARIO(NOMBRE, AMATERNO, APATERNO, PUESTO) VALUES('Axa', 'Zarate', 'Garcia', 'Mesero');
